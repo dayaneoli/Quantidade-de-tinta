4872ad50efe9a1cfc0374391ea678bab4c79dbcc
dayaneoli <100381919+dayaneoli@users.noreply.github.com>
2023-01-12 22:26:25 -0300

    Add files via upload

---
+++ ex011.py
@@ -0,0 +1,12 @@
+l = float((input('Digite a largura da parede em metros ')))
+a = float((input("Digite a altura da parede em metros ")))
+c = (l * a)
+t = (c / 2)
+print("A quantidade de tinta necessária para pintar a área de {:.2f} m² de parede é {:.2f} litros".format(c, t))
+
+
+
+
+
+
+
